da8afbed481441a7fc5462716995415d48039cd6
stefzouille <contact.lokcy@gmail.com>
2021-02-26 22:48:14 +0100

    add

---
+++ exo-loops-iterations/onlyMultipleOf3And7.js
@@ -0,0 +1,7 @@
+for (let i = 1000; i > 0; i -= 3) {
+  
+if (i === 3 ) {
+
+}
+console.log(i)
+}
---
+++ exo-loops-iterations/sumAll2D.js
@@ -0,0 +1,16 @@
+//Ecrivez un programme qui affiche la somme de tous les nombres présents dans ce tableau (qui contient des tableaux)
+
+let tab = [
+  [1, 2, 3],
+  [4, -5, 7],
+  [-3, -6],
+  [10, -13],
+]
+
+for (let i = 0; i < tab.length; ++i) {
+  console.log(`Array ${i + 1}:`)
+  for (let j = 0; j < tab[i].length; ++j) {
+    console.log(`\t${tab[i][j]}`)
+  
+  }
+}

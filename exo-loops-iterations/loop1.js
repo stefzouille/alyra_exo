ec75d95e7587955e1af1c631eebfed7dcd8801aa
stefzouille <contact.lokcy@gmail.com>
2021-02-26 20:01:03 +0100

    add

--- exo-loops-iterations/loop1.js
+++ exo-loops-iterations/loop1.js
@@ -1,22 +1,12 @@
 let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 let nb = 0
-while ( nb <= 9){
+while ( nb < tab.length) {
 nb ++
 console.log(nb)
 }
 
 
-// console.log(tab[0])
-// console.log(tab[1])
-// console.log(tab[2])
-// console.log(tab[3])
-// console.log(tab[4])
-// console.log(tab[5])
-// console.log(tab[6])
-// console.log(tab[7])
-// console.log(tab[8])
-// console.log(tab[9])
 
 
 

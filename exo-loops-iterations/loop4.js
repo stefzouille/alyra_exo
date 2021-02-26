443fbb022184d804e4ea89825025fe679ca9aa33
stefzouille <contact.lokcy@gmail.com>
2021-02-26 21:07:18 +0100

    add

---
+++ exo-loops-iterations/loop4.js
@@ -0,0 +1,8 @@
+//Avec une boucle for-of afficher les éléments du tableau suivant, ligne par ligne:
+
+let tab = ['Js', 'Solidity', 'Html', 'Css', 'Python', 'Java', 'C++', 'Rust']
+
+
+for (let elem of tab) {
+  console.log(elem)
+}

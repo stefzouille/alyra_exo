073fda8e2958a61c427349d52e77ef67b7aa975c
stefzouille <contact.lokcy@gmail.com>
2021-02-26 21:12:42 +0100

    add

--- exo-loops-iterations/loop5.js
+++ exo-loops-iterations/loop5.js
@@ -1,3 +1,7 @@
 //Avec la méthode forEach afficher les éléments du tableau suivant, ligne par ligne:
 
 let tab = ['map', 'filter', 'forEach', 'reduce', 'every']
+
+tab.forEach((elem) => {            // attention a la double parenthese apres .forEach((elem) => { console.log(elem)})
+  console.log(elem)
+})

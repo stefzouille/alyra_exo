e58fd46efeb2e438542721ff5fe9e6778159377b
stefzouille <contact.lokcy@gmail.com>
2021-02-27 21:14:53 +0100

    add

--- exo-loops-iterations/sumAll2D.js
+++ exo-loops-iterations/sumAll2D.js
@@ -1,18 +1,32 @@
 //Ecrivez un programme qui affiche la somme de tous les nombres présents dans ce tableau (qui contient des tableaux)
 
 let tab = [
-  [1, 2, 3],
-  [4, -5, 7],
-  [-3, -6],
-  [10, -13],
+  [1, 2, 3],         //6
+  [4, -5, 7],        //6
+  [-3, -6],          //-9
+  [10, -13],          //-3
 ]
 
+ let c0 = tab[0][0] + tab[0][1] + tab[0][2]
+ let c1 = tab[1][0] + tab[1][1] + tab[1][2]
+ let c2 = -tab[2][0] + tab[2][1] 
+ let c3 = tab[2][0] + tab[2][1] 
+                                  //c0 + c1 = 12
+ let sum = c0 + c1 + c2 + c3      //c2 + c3 = -12
+               
+
+console.log(sum)
 
 
-for (let i = 0; i < tab.length; ++i) {
-  console.log(`Array ${i + 1}:`)
-  for (let j = 0; j < tab[i].length; ++j) {
-    console.log(`\t${tab[i][j]}`)
+
+// let a = [1, 2, 3]
+// let b = a[2] * 3 + 4 / 2
+
+
+// for (let i = 0; i < tab.length; ++i) {
+//   console.log(`Array ${i + 1}:`)
+//   for (let j = 0; j < tab[i].length; ++j) {
+//     console.log(`\t${tab[i][j]}`)
   
-  }
-}
+//   }
+// }

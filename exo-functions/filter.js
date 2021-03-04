27b6f1acb7ad855df07e525ae81bbf0795ee578e
stefzouille <contact.lokcy@gmail.com>
2021-03-04 22:29:29 +0100

    .

--- exo-functions/filter.js
+++ exo-functions/filter.js
@@ -6,17 +6,38 @@
 // expected output: Array ["exuberant", "destruction", "present"]
 
 
- const filter = (words, fct, tab) => {
-   const newArray = []
-    for (const elem of words) {
-    newArray.push(fct(elem))
-    return newArray
-    }
-   result = words.filter(word => word.length > 6)  
-      return result = words.filter(fct(newArray[tab]))
+//  const filter = (words, fct, []) => {
+//    const newArray = []
+//     for (const elem of words) {
+//     newArray.push(fct(elem))                         //fct is not a function !
+//         }
+//    result = words.filter(word => word.length > 6)  
+//       return result = words.filter(fct(newArray[tab]))
    
-  }
-  console.log(filter(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']))
+//   }
+//   console.log(filter(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']))
 
   
 // I dry like a piece of wood in the oven
+
+const bug = (word) => {
+  if ( word.length >= 6){
+    return true
+  } else {
+    return false
+  }
+
+}
+
+const filter = (tab, fct) => {
+  let newArray = []
+  for ( let i = 0; i < tab.length; i++){
+    if (fct(tab[i])) {
+        newArray.push(tab[i])
+    }
+  }
+return newArray
+}
+console.log(filter(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'], bug))
+
+//console.log(filter(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']))

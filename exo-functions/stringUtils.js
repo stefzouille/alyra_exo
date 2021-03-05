260093f2054d52e0694b314262bbc979ecbba732
stefzouille <contact.lokcy@gmail.com>
2021-03-05 10:13:12 +0100

    .

--- exo-functions/stringUtils.js
+++ exo-functions/stringUtils.js
@@ -3,6 +3,17 @@
 
   
  function reverseString(str) {
-    return str.split('').reverse().join("")
+    return str.split('').reverse().join("")  //reverse ne fonctionne que sur un tableau
 }
+
+const isPalindrome = (str) => {
+    if (reverseString(str) === str ) {
+        return true
+     } else {
+            return false
+        }
+    
+}
+
 console.log(reverseString("hello!"))
+console.log(isPalindrome('radar'))

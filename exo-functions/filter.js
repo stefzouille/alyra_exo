35fe51d05e5af7ee8c58fd9be044de53218d7dac
stefzouille <contact.lokcy@gmail.com>
2021-03-05 12:55:45 +0100

    .

--- exo-functions/filter.js
+++ exo-functions/filter.js
@@ -21,16 +21,18 @@
 isEven = (number) => {
     return number % 2 === 0 ? true : false
 }
-
-let filter = (func, array) => {
-    let virginTab = []
-    for (let i = 0; i < array.length; i++) {
-        func(array[i]) ? virginTab.push(array[i]) : ''
+const filter = (func, array) => {
+    const filteredTab = []
+    for (const elem of array) {
+        if (func(elem)) {
+            filteredTab.push(elem)
+        } 
     }
-    return virginTab
+    return filteredTab
 }
-
-
 numbers = [12, 1, 43, 3, 8, 9, 22, 6, 19, 88, 76]
 
+
+//let newTab = numbers.filter(isEven)
+//console.log(newTab)
 console.log(filter(isEven, numbers))
